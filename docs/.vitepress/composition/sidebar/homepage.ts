d51fb8ec6cfe372df43283809feb694b6d3d9b36
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2024-07-03 15:28:11 +0900

    docs: 护照更新流程

---
+++ docs/.vitepress/composition/sidebar/homepage.ts
@@ -0,0 +1,111 @@
+import type { DefaultTheme } from 'vitepress'
+
+export function useHomepageSidebar(): DefaultTheme.Sidebar {
+  return {
+    '/zh/': zhSidebar(),
+    '/ja/': jaSidebar(),
+  }
+}
+
+const zhSidebar = (): DefaultTheme.SidebarItem[] => {
+  return [
+    {
+      text: '首页',
+      items: [{ text: '简介', link: '/' }],
+    },
+
+    {
+      text: '编程',
+      items: [
+        {
+          text: '编程语言',
+          link: '/zh/programming/language/',
+        },
+
+        {
+          text: '数据库',
+          link: '/zh/programming/database/',
+        },
+
+        {
+          text: '资源管理工具',
+          link: '/zh/programming/source-manage-tool/',
+        },
+
+        {
+          text: '备忘录',
+          link: '/zh/programming/coding-memo/maven-repository',
+        },
+      ],
+    },
+
+    {
+      text: '马克思主义',
+      items: [
+        {
+          text: '读书笔记',
+          link: '/zh/marxism/reading-note/',
+        },
+
+        {
+          text: '读书报告',
+          link: '/zh/marxism/reading-report/',
+        },
+      ],
+    },
+
+    {
+      text: '其他',
+      collapsed: true,
+      items: [
+        {
+          text: '护照更新流程',
+          link: '/zh/other/passport-renewal',
+        },
+      ],
+    },
+  ]
+}
+
+const jaSidebar = (): DefaultTheme.SidebarItem[] => {
+  return [
+    {
+      text: 'トップ',
+      items: [{ text: '概要', link: '/' }],
+    },
+
+    {
+      text: 'プログラミング',
+      items: [
+        {
+          text: 'プログラミング言語',
+          link: '/ja/programming/language/',
+        },
+        {
+          text: '試験準備',
+          link: '/ja/programming/exam-preparation/',
+        },
+
+        {
+          text: '備忘録',
+          link: '/ja/programming/coding-memo/maven-repository',
+        },
+      ],
+    },
+
+    {
+      text: 'マルクス主義',
+      items: [
+        {
+          text: '読書ノート',
+          link: '/ja/marxism/reading-note/',
+        },
+
+        {
+          text: '読書レポート',
+          link: '/ja/marxism/reading-report/',
+        },
+      ],
+    },
+  ]
+}
--- docs/.vitepress/composition/sidebar/index.ts
+++ docs/.vitepress/composition/sidebar/index.ts
@@ -1,9 +1,10 @@
 import type { DefaultTheme } from 'vitepress'
 import { useDatabaseSidebar } from './database'
+import { useHomepageSidebar } from './homepage'
 
 export default function useSidebar(): DefaultTheme.Sidebar {
   return {
-    ...useHomePageSidebar(),
+    ...useHomepageSidebar(),
 
     // 编程
     ...useProgrammingLanguageSidebar(),
@@ -15,98 +16,6 @@ export default function useSidebar(): DefaultTheme.Sidebar {
     // 马克思主义
     ...useMarxismReadingNoteSidebar(),
     ...useMarxismReadingReportSidebar(),
-  }
-}
-
-function useHomePageSidebar(): DefaultTheme.Sidebar {
-  return {
-    '/zh/': [
-      {
-        text: '首页',
-        items: [{ text: '简介', link: '/' }],
-      },
-
-      {
-        text: '编程',
-        items: [
-          {
-            text: '编程语言',
-            link: '/zh/programming/language/',
-          },
-
-          {
-            text: '数据库',
-            link: '/zh/programming/database/',
-          },
-
-          {
-            text: '资源管理工具',
-            link: '/zh/programming/source-manage-tool/',
-          },
-
-          {
-            text: '备忘录',
-            link: '/zh/programming/coding-memo/maven-repository',
-          },
-        ],
-      },
-
-      {
-        text: '马克思主义',
-        items: [
-          {
-            text: '读书笔记',
-            link: '/zh/marxism/reading-note/',
-          },
-
-          {
-            text: '读书报告',
-            link: '/zh/marxism/reading-report/',
-          },
-        ],
-      },
-    ],
-
-    '/ja/': [
-      {
-        text: 'トップ',
-        items: [{ text: '概要', link: '/' }],
-      },
-
-      {
-        text: 'プログラミング',
-        items: [
-          {
-            text: 'プログラミング言語',
-            link: '/ja/programming/language/',
-          },
-          {
-            text: '試験準備',
-            link: '/ja/programming/exam-preparation/',
-          },
-
-          {
-            text: '備忘録',
-            link: '/ja/programming/coding-memo/maven-repository',
-          },
-        ],
-      },
-
-      {
-        text: 'マルクス主義',
-        items: [
-          {
-            text: '読書ノート',
-            link: '/ja/marxism/reading-note/',
-          },
-
-          {
-            text: '読書レポート',
-            link: '/ja/marxism/reading-report/',
-          },
-        ],
-      },
-    ],
   }
 }
 
